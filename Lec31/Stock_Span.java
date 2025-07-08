65167ef25b564dd3cbd8b8c612736ca5472b5f8a
monukumar98 <monu012as@gmail.com>
2025-07-08 20:57:11 +0530

    monotonic Stack

---
+++ Lec31/Stock_Span.java
@@ -0,0 +1,35 @@
+package Lec31;
+
+import java.util.Stack;
+
+public class Stock_Span {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 30, 35, 40, 38, 35 };
+		Cal_Span(arr);
+	}
+
+	private static void Cal_Span(int[] arr) {
+		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack<>();
+		int[] ans = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+				st.pop();
+			}
+			// span calculate krne ka
+			if (st.isEmpty()) {
+				ans[i] = i + 1;
+			} else {
+				ans[i] = i - st.peek();
+			}
+			st.push(i);
+		}
+		for (int i = 0; i < ans.length; i++) {
+			System.out.print(ans[i] + " ");
+		}
+
+	}
+
+}
