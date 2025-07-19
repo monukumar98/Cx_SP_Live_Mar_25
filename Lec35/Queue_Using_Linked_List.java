a763e3a17dc33c4f22db718f2b1b1e650aae58c8
monukumar98 <monu012as@gmail.com>
2025-07-19 18:47:59 +0530

    LinkedlIst Qps

--- Lec35/Queue_Using_Linked_List.java
+++ Lec35/Queue_Using_Linked_List.java
@@ -7,16 +7,16 @@ public class Queue_Using_Linked_List {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//LinkedList<Integer> q = new LinkedList<>();
+		// LinkedList<Integer> q = new LinkedList<>();
 		Queue<Integer> q = new LinkedList<>();
-		q.add(10);//add last--> Enqueue
+		q.add(10);// add last--> Enqueue
 		q.add(20);
 		q.add(40);
 		q.add(30);
 		System.out.println(q);
 		System.out.println(q.poll());// remove first --> Dequeue
 		System.out.println(q);
-		System.out.println(q.peek());
+		System.out.println(q.peek());// get first --> getfornt
 
 	}
 
