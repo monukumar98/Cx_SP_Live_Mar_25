8ec93e58b23a48af7c0f763fbfa9652275720909
monukumar98 <monu012as@gmail.com>
2025-07-23 21:44:19 +0530

    Binary Tree QPS

--- Lec37/LCA.java
+++ Lec37/LCA.java
@@ -13,31 +13,22 @@ public class LCA {
 
 	class Solution {
 		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-			if(root==null) {
+			if (root == null) {
 				return null;
 			}
-			if(root==p || root==q) {
+			if (root == p || root == q) {
 				return root;
 			}
-			TreeNode left =lowestCommonAncestor(root.left, p, q);
-			TreeNode right =lowestCommonAncestor(root.right, p, q);
+			TreeNode left = lowestCommonAncestor(root.left, p, q);
+			TreeNode right = lowestCommonAncestor(root.right, p, q);
+			if (left != null && right != null) {
+				return root;
+			} else if (left == null) {
+				return right;
+			} else {
+				return left;
+			}
 
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
